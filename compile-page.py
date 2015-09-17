302335eacba72e857feaab056033e293b1a1c8e6
Teemu Autto <teemu@rautakuu.org>
2015-09-17 23:48:46 +0300

    Detect face using external script

--- compile-page.py
+++ compile-page.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 from kayfabe import session
 from kayfabe.models import *
@@ -13,7 +14,8 @@ import logging, sys
 from datetime import date
 from os import path
 from urllib.request import urlretrieve
-from PIL import Image
+from PIL import Image, ImageOps
+import json
 
 from subprocess import call, check_output
 
@@ -29,6 +31,9 @@ def get_face(wrestler):
         
         http://www.cagematch.net/site/main/img/portrait/00000293.jpg
     '''
+    
+    thumb_size = (124,124)
+
     w = 'ass/w/{:0>8}.jpg'.format(wrestler.nr)
     f = 'ass/f/{:0>8}.jpg'.format(wrestler.nr)
     if not path.isfile(f):
@@ -41,18 +46,53 @@ def get_face(wrestler):
                     if not src:
                         raise RuntimeError('No picture found')
                     urlretrieve(src, w)
-                except:
+                except e:
+                    logging.warning('Could not crop face: %s' % e)
                     r = Bing().imageSearch(u'%s wrestler' % wrestler.name, ImageFilters="'Face:Face'")
                     urlretrieve(r[0]['Thumbnail']['MediaUrl'], w)
+            try:
+                im = crop_face(w, thumb_size)
+            except:
+                
+                im = Image.open(w)
+                im = square_image(im, thumb_size)
+                #im = im.convert('RGB').convert('P', palette=Image.ADAPTIVE, colors=255)
 
-            im = Image.open(w)
-            im = square_image(im, (64,64))
-            #im = im.convert('RGB').convert('P', palette=Image.ADAPTIVE, colors=255)
+
             im.save(f, 'JPEG')
         except:
             f = None
 
     return f
+
+def crop_face(picture, size=(64,64)):
+    x,y,w,h = find_face(picture)
+
+    im = Image.open(picture)
+
+    im = im.crop((x, y, x+w, y+h))
+    im.thumbnail(size)
+
+    #im = ImageOps.fit(im, (64,64), Image.ANTIALIAS, 0.2, centering=(left,top))
+    ##image = ImageOps.fit(image, thumbnail_size, Image.ANTIALIAS)
+    #im = im.transform((64, 64), im.EXTENT, (x, y, x+w, y+h))
+    return im
+
+def find_face(picture):
+    data = check_output(["python2", "face-detect.py", picture]).decode('utf-8')
+    faces = json.loads(data)
+
+    max_idx = None
+    max_size = 0
+    for i, face in enumerate(faces):
+        face_size = face[2] * face[3]
+        if face_size > max_size:
+            max_size = face_size
+            max_idx = i
+
+#    print('Biggest face', max_idx, faces[max_idx])
+
+    return faces[max_idx]
 
 def get_wrestlers(from_date=date.today()):
 
@@ -61,7 +101,7 @@ def get_wrestlers(from_date=date.today()):
     a = session.query(Score).order_by(desc(func.max(Score.score))).\
         join(Match).filter(Match.date >= since).\
         join(Wrestler).\
-        group_by(Score.wrestler_nr).slice(0,26).all()
+        group_by(Score.wrestler_nr).slice(0,100).all()
 
     r = []
 
@@ -72,6 +112,11 @@ def get_wrestlers(from_date=date.today()):
     return r
 
 if __name__ == '__main__':
+
+    config={
+        'items_per_page': 20
+    }
+
     logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
     wd = WikiData()
@@ -88,5 +133,6 @@ if __name__ == '__main__':
 
     print(tpl.get_template('index.tpl.html').render(
         promotions=promotions,
-        wrestlers=wrestlers
+        wrestlers=wrestlers,
+        config=config
     ))
