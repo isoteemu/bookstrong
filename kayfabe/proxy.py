47360ecd6c4f81e5cd3ef199f50338cb349de7be
Teemu Autto <teemu@rautakuu.org>
2017-03-17 14:56:28 +0200

    Search wikidata for wrestler name.

--- kayfabe/proxy.py
+++ kayfabe/proxy.py
@@ -3,17 +3,20 @@
 
 from kayfabe import session
 from kayfabe.models import *
-from kayfabe.scrapper import cagematchnet
+from kayfabe.scrapper import WikiData, cagematchnet
 
 from sqlalchemy.orm import exc
 
 import logging
+logger = logging.getLogger(__name__)
 
 class ModelProxy():
     force_update = False
 
 class WrestlerProxy(ModelProxy):
-    ''' Proxy for wrestler info scraping. '''
+    ''' Proxy for wrestler info scraping. 
+    TODO: Move into setter/getter
+    '''
 
     _cagematch = None
     wrestler = None
@@ -65,7 +68,7 @@ class WrestlerProxy(ModelProxy):
             promotion = Promotion(cm_id=promotion_id, name=promotion_data['name'],
                                   abbrevation=promotion_data['abbrevation'])
 
-            logging.debug("Creating new Promotion: %s", promotion)
+            logger.debug("Creating new Promotion: %s", promotion)
             session.add(promotion)
 
         return promotion
@@ -78,9 +81,45 @@ class WrestlerProxy(ModelProxy):
 
         if not wrestler.name or force_update:
             data = self.cagematch(wrestler.nr)
-            wrestler.name = data.get('name', wrestler.name)
+            wrestler.name = data.get('name', wrestler.name).strip()
+
+            # Causes -sometimes- memory violation on my python3.6m1
+            '''
+            gmk = self._gimmick(wrestler.name, wrestler)
+            gmk.primary = True
+            '''
+            wd_name = self._wikidata_name(wrestler)
+            if wd_name:
+                wrestler.name = wd_name
+                logger.debug('Set name from WikiData: %s', wd_name)
 
         return wrestler.name
+
+    def _wikidata_name(self, wrestler: Wrestler):
+        ''' Search wikidata for wrestler real name. '''
+        wd_name = None
+        try:
+            data = WikiData().search_wrestler(wrestler.name)
+            wd_name = data.get('labels', {}).get('en', {}).get('value', None).strip()
+            logger.debug("Found name from WikiData: %s", wd_name)
+        except (KeyError, AttributeError):
+            pass
+
+        return wd_name
+
+    def _gimmick(self, gimmick: str, wrestler: Wrestler) -> Gimmick:
+        logger.debug("Looking for gimmick %s", gimmick)
+        '''Return gimmick model. Create one if necessary.'''
+        try:
+            gmk = session.query(Gimmick).filter_by(gimmick=gimmick, wrestler_nr=wrestler.nr).one()
+        except exc.NoResultFound:
+            logger.debug('Creating new gimmick "%s"', gimmick)
+            gmk = Gimmick(gimmick=gimmick, wrestler_nr=wrestler.nr)
+
+            wrestler.gimmicks.append(gmk)
+            session.add(gmk)
+
+        return gmk
 
     def gimmicks(self, force_update=None):
         ''' Get wrestler gimmicks '''
@@ -90,28 +129,24 @@ class WrestlerProxy(ModelProxy):
         wrestler = self.wrestler
 
         if not wrestler.gimmicks or force_update:
+
             data = self.cagematch(wrestler.nr)
 
             for gimmick in data['gimmicks']:
+                logger.debug("Cagematch gimmick: %s", gimmick)
 
                 if type(gimmick) == str:
                     parent = None
                 else:
                     parent, gimmick = gimmick
+                    parent = parent.strip()
 
                 gimmick = gimmick.strip()
 
-                try:
-                    gmk = session.query(Gimmick).filter_by(gimmick=gimmick,
-                                                           wrestler_nr=wrestler.nr).one()
-                except exc.NoResultFound:
-                    logging.debug('Creating new gimmick "%s"', gimmick)
-                    gmk = Gimmick(gimmick=gimmick)
-                    wrestler.gimmicks.append(gmk)
+                gmk = self._gimmick(gimmick, wrestler)
 
                 if parent:
-                    alias_of = session.query(Gimmick).filter_by(gimmick=parent,
-                                                                wrestler_nr=wrestler.nr).one()
+                    alias_of = self._gimmick(parent, wrestler)
 
                     gmk.alias_of = alias_of.id
 
@@ -119,4 +154,5 @@ class WrestlerProxy(ModelProxy):
                     gmk.primary = True
 
             ## TODO: Remove dead ones 
+
         return wrestler.gimmicks
