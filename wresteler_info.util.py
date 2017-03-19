e9b77e2fad98d1f1ffed81da9a67781153237cce
Teemu Autto <teemu@rautakuu.org>
2017-03-19 16:56:16 +0200

    Make wrestler accept multiple wrestler_nr's.

--- wresteler_info.util.py
+++ wresteler_info.util.py
@@ -13,6 +13,16 @@ from kayfabe.view import get_image_path
 import logging, sys
 
 from sqlalchemy import asc, desc, func
+from sqlalchemy.orm import exc
+
+
+def get_wrassler(nr):
+    try:
+        wrestler = session.query(Wrestler).filter_by(nr=nr).one()
+    except exc.NoResultFound:
+        wrestler = Wrestler(nr=args.wrestler_id)
+        session.add(wrestler)
+    return wrestler
 
 
 if __name__ == '__main__':
@@ -21,7 +31,7 @@ if __name__ == '__main__':
     cmdline = argparse.ArgumentParser(description='Find wrestler info from different datasources.')
 
     cmdline.add_argument('wrestler_id', help='Wrestler ID number.',
-                         type=int, nargs='?')
+                         type=int, nargs='*')
 
     cmdline.add_argument('--name', help="Select wrestler by name.")
 
@@ -29,12 +39,12 @@ if __name__ == '__main__':
 
     cmdline.add_argument('--debug', help='Debug', action='store_true')
     cmdline.add_argument('--update', help='Force updating info', action='store_true')
+    cmdline.add_argument('--picture', help='Get picture.', action='store_true')
 
     args = cmdline.parse_args()
 
     if args.debug:
-        logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-
+        logging.basicConfig(stream=open('log.txt', 'w'), level=logging.DEBUG)
     else:
         logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
@@ -42,56 +52,56 @@ if __name__ == '__main__':
 
     force_update = args.update
 
-    if args.add:
+    wrestlers = []
 
-        if args.wrestler_id:
-            wrestler = Wrestler(nr=args.wrestler_id)
-        elif args.name:
+    if args.wrestler_id:
+        for nr in args.wrestler_id:
+            wrestlers.append(get_wrassler(nr))
+
+    elif args.name:
+        wrestler = session.query(Wrestler).filter_by(name=args.name).one()
+        if wrestler:
+            wrestlers.append(wrestler)
+        else:
             _wrestlers = cagematchnet.search(args.name.strip())
             if len(_wrestlers) == 1:
                 w_nr = list(_wrestlers.keys())[0]
-                wrestler = Wrestler(nr=w_nr)
+                wrestlers.append(get_wrassler(w_nr))
                 logger.info('Found Wrestler %s from cagematch.net', w_nr)
             else:
                 logging.error('Wrong ammount of results %i', len(_wrestlers))
                 sys.exit(2)
-        else:
-            print('Missing name or id.')
-            sys.exit(2)
 
-        session.add(wrestler)
-        logging.info('Adding wrestler %s with id %i' % (wrestler.name, wrestler.nr))
-        force_update = True
-
-    elif args.name:
-        wrestler = session.query(Wrestler).filter_by(name=args.name).one()
     else:
-        wrestler = session.query(Wrestler).get(args.wrestler_id)
-
-    proxy = WrestlerProxy(wrestler)
-    proxy.force_update = force_update
-
-    print("Name: %s" % proxy.name(force_update))
-    print("Id: %s" % wrestler.nr)
-
-    logging.debug('Retrieving gimmicks')
-    gimmicks = proxy.gimmicks(force_update)
-    logging.debug('Flattening gimmicks')
-
-    ringnames = [g.gimmick for g in gimmicks]
-    print("Ringnames: %s" % ', '.join(ringnames))
-
-    print("Promotion: %s" % proxy.promotion(force_update).name)
-
-    try:
-        print("Last match: %s" % get_last_match(wrestler).date)
-    except:
-        pass
-
-    picture = get_image_path(wrestler)
-    print('Picture: %s' % picture)
-
-
+        wrestlers = session.query(Wrestler).filter(Wrestler.nr < 1000000).order_by(Wrestler.nr)
 
     if args.add or args.update:
-        session.commit()
+        force_update = True
+
+    for wrestler in wrestlers:
+
+        proxy = WrestlerProxy(wrestler)
+        proxy.force_update = force_update
+
+        print("Id: %s" % wrestler.nr)
+        print("Name: %s" % proxy.name(force_update))
+
+        gimmicks = proxy.gimmicks(force_update)
+        ringnames = [g.gimmick for g in gimmicks]
+        print("Ringnames: %s" % ', '.join(ringnames))
+
+        print("Promotion: %s" % proxy.promotion(force_update).name)
+
+        try:
+            print("Last match: %s" % get_last_match(wrestler).date)
+        except:
+            pass
+
+        if args.picture:
+            picture = get_image_path(wrestler)
+            print('Picture: %s' % picture)
+
+        if force_update:
+            print('')
+            logger.debug("Storing")
+            session.commit()
